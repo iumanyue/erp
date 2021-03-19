4bcc5eac5e39df664d082e9125a15f03d4edf11e
iumanyue <582966488@qq.com>
2021-03-19 17:08:19 +0800

    login

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import ElementUi from 'element-ui'
+import router from './router'
+import 'element-ui/lib/theme-chalk/index.css'
 // import axios from 'axios'
 
+import './assets/css/global.css'
 
 Vue.use(ElementUi)
 
@@ -10,4 +13,5 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
