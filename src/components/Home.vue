b14c52f75d07f3e3a6adec857b7ac4e472c091ca
iumanyue <582966488@qq.com>
2021-03-22 17:36:55 +0800

    111

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,17 @@
 <template>
-    <div>
-        <el-button type='info' @click="loginOut">退出</el-button>
-    </div>
+        <el-container class="home-container">
+            <el-header>
+                <div>
+                    <img src="~assets/logo.jpg" alt="" class="logos">
+                    <span class="title">IUmanyueLzn</span>
+                </div>
+                <el-button type='info' @click="loginOut" class="logout">退出</el-button>
+            </el-header>
+            <el-container>
+                <el-aside width="200px">Aside</el-aside>
+                <el-main>Main</el-main>
+            </el-container>
+        </el-container>
 </template>
 
 <script>
@@ -22,5 +32,11 @@ export default {
 </script>
 
 <style scoped>
-
+.home-container{height: 100%;}
+.el-header{background-color: #373d41;display: flex;justify-content: space-between;align-items:center;}
+.el-aside{background-color: #333744;}
+.el-main{background-color: #eee;}
+.logos{width: 60px;float: left;}
+.title{padding:0 10px;font-size: 20px;color: #ffffff;display: block;float: left;height: 60px;line-height: 60px;}
+.logout{}
 </style>
