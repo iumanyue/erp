0bdb706daaf9793d50798077c9b5787ce7a72046
iumanyue <582966488@qq.com>
2021-03-28 13:08:46 +0800

    jiali

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -58,7 +58,7 @@
     <el-dialog
       title="添加用户"
       :visible.sync="addDialogVisible"
-      width="50%">
+      width="50%" @close="addDialogClosed">
       <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="70px">
         <el-form-item label="用户名" prop="username">
           <el-input v-model="addForm.username"></el-input>
@@ -75,7 +75,7 @@
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="addDialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
+        <el-button type="primary" @click="addUser">确 定</el-button>
       </span>
     </el-dialog>
 
@@ -89,6 +89,25 @@
 export default {
   name:'User',
   data(){
+    // 校验邮箱手机号
+    var checkEmail = (rule,value,callback)=>{
+        const regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/
+        if(regEmail.test(value)){
+          return callback()
+        }
+        callback(new Error('请输入合法的邮箱'))
+    }
+
+    var checkMobile = (rule,value,callback)=>{
+         const regMobile = /^(0|86|17951)?(13[0-9]|15[0123456789]|17[678]|18[0-9]|14[57])[0-9]{8}$/
+
+        if(regMobile.test(value)){
+          return callback()
+        }
+        callback(new Error('请输入合法的手机号'))
+    }
+
+
    return {
     //  获取用户列表的参数对象
     queryInfo:{
@@ -119,11 +138,12 @@ export default {
       ],
       email:[
         {required: true, message: '请输入邮箱', trigger: 'blur' },
-        {min:6,max:15,message:'邮箱的长度再6~15个字符之间',trigger: 'blur' }
+        {validator:checkEmail,trigger:'blur'}
       ],
       mobile:[
-        {required: true, message: '请输入手机号', trigger: 'blur' },
-        {min:11,max:11,message:'手机号的长度11字符',trigger: 'blur' }
+        {required: true, message: '请输入手机号', trigger: 'blur' }
+,
+        {validator:checkMobile,trigger:'blur'}
       ]
     }
    }
@@ -160,6 +180,26 @@ export default {
        return this.$message.error('更新用户状态失败')
      }
      this.$message.success('更新状态成功')
+    },
+    // duihuak 关闭
+    addDialogClosed(){
+        this.$refs.addFormRef.resetFields()
+    },
+    addUser(){
+      this.$refs.addFormRef.validate(async valid =>{
+        if(!valid) return
+
+        // 直接发起添加用户的网络请求
+         const{data:res} = await this.$http.post('users',this.addForm)
+
+            if(res.meta.status !==201){
+              this.$message.error('添加用户失败')
+            }
+            this.$message.success('添加用户成功')
+            // 隐藏添加用户的对话框
+            this.addDialogVisible = false
+            this.getUserLists()
+      })
     }
   },
 }
